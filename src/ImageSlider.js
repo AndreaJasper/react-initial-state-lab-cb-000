505e217e45fd3aa176bc7d82c5aea1244bb079ff
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-04-23 18:23:13 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
 class ImageSlider extends React.Component {
-  constructor() {
-    super();
-      this.state= {
-        currentSlideIndex: 0
-      }
-  }
+  state = {currentSlideIndex: 0}
   
    render() {
     return(
       <div>
-        <p>I am on slide {this.state.currentSlideIndex}, where {this.state.currentSlideIndex} is the value of {this.state.currentSlideIndex}.</p>
+        I am on slide {this.state.currentSlideIndex}
       </div>
     );
   }
